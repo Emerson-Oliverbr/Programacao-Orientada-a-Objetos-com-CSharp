547e9f44ef4dcfabf6acc6bbe3a716e93fa1129c
Emerson Oliveira <emerson.oliverbr@gmail.com>
2026-01-30 20:05:00 -0300

    Vetores parte 1

---
+++ 3.Arrays e Listas/VetoresParte1/VetoresParte1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace VetoresParte1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        double[] valores = new double[n];
+
+        for(int i=0; i<n; i++)
+        {
+            valores[i] = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        }
+
+        double alturaMedia = 0.0;
+        for (int i=0; i<n; i++)
+        {
+            alturaMedia += valores[i] / n;
+        }
+        Console.WriteLine($"Altura Média = {alturaMedia.ToString("F2", CultureInfo.InvariantCulture)}");
+    }
+}
