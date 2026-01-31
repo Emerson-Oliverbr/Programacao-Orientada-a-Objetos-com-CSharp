666e978f34fe0fc90e114e4731dfd1d46f02d948
Emerson Oliveira <emerson.oliverbr@gmail.com>
2026-01-31 13:29:00 -0300

    Vetores parte 2

---
+++ 3.Arrays e Listas/VetoresParte2/VetoresParte2/Product.cs
@@ -0,0 +1,6 @@
+﻿namespace VetoresParte2;
+public class Product
+{
+    public string? Name { get; set; }
+    public double Price { get; set; }
+}
---
+++ 3.Arrays e Listas/VetoresParte2/VetoresParte2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace VetoresParte2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Product[] vect = new Product[n];
+
+        for(int i=0; i<n; i+=1)
+        {
+            string name = Console.ReadLine();
+            double price = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            vect[i] = new Product { Name = name, Price = price };
+        }
+
+        double sum = 0.0;
+
+        for (int i=0; i<n; i+=1)
+        {
+            sum += vect[i].Price;
+        }
+
+        double precoMedio = sum / vect.Length;
+
+        Console.WriteLine($"Preco Médio = R$ {precoMedio.ToString("F2",CultureInfo.InvariantCulture)}");
+    }
+    
+}
