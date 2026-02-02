6ec10ae962003fd5aa23c928999c31ff8e0b7fb8
Emerson Oliveira <emerson.oliverbr@gmail.com>
2026-02-02 16:26:42 -0300

    Add: Modificador Params

---
+++ 3.Arrays e Listas/ModificadorParams/ModificadorParams/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ModificadorParams;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //int s1 = Calculadora.Soma(2, 3);
+        //int s2 = Calculadora.Soma(3, 6, 1);
+
+        //int s1 = Calculadora.Soma(new int[] { 2, 3 });
+        //int s2 = Calculadora.Soma(new int[] { 2, 3, 5 });
+
+        int s1 = Calculadora.Soma(2, 3 );
+        int s2 = Calculadora.Soma( 2, 3, 5 );
+
+        Console.WriteLine(s1);
+        Console.WriteLine(s2); 
+    }
+}
