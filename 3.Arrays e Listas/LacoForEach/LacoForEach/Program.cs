2d02edb6fba19ddc0473f1a5b262ca0c21e177d4
Emerson Oliveira <emerson.oliverbr@gmail.com>
2026-02-02 17:13:12 -0300

    Add: foreach

---
+++ 3.Arrays e Listas/LacoForEach/LacoForEach/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LacoForEach;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        string[] vect = new string[] { "Daniela", "Emerson", "Laura" };
+
+        foreach(string obj in vect)
+        {
+            Console.WriteLine(obj);
+        }
+    }
+}
