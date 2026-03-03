291696d82b2119616b801eae401f37e02e41ad02
Emerson Oliveira <emerson.oliverbr@gmail.com>
2026-03-02 23:49:59 -0300

    Aprendendo enumeracoes

---
+++ Modulo-5-Enumeracao-e-Composicao/Enumeracoes/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿namespace Enumeracoes.Entities;
+using Enumeracoes.Entities.Enums;
+
+internal class Order
+{
+    public int Id { get; set; }
+    public DateTime Moment { get; set; }
+    public OrderStatus Status { get; set; }
+
+    public override String ToString()
+    {
+        return $"Id: {Id}, Momento:{Moment}, Status: {Status}";
+    }
+}
---
+++ Modulo-5-Enumeracao-e-Composicao/Enumeracoes/Program.cs
@@ -0,0 +1,27 @@
+﻿using Enumeracoes.Entities;
+using Enumeracoes.Entities.Enums;
+
+namespace Enumeracoes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Order order = new Order 
+            { Id = 1080,
+              Moment = DateTime.Now,
+              Status = OrderStatus.PagamentoPendente
+            };
+        Console.WriteLine($"Status do pedido: {order}");
+
+        //Conversões
+
+        string txt = OrderStatus.PagamentoPendente.ToString();
+        Console.WriteLine(txt);
+
+        OrderStatus os = Enum.Parse<OrderStatus>("Entregue");
+        Console.WriteLine(os);
+
+        Console.ReadKey();
+    }
+}
